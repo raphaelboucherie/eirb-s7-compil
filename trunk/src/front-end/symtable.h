c9a85a8516830f5c0324ce30784f8e9ffd64e661
laturaze.paul@gmail.com <laturaze.paul@gmail.com@a4e27ffd-a61e-4b3a-5b61-575e9ed28477>
2012-01-08 13:38:58 +0000

    Modification du module de table des symboles

--- trunk/src/front-end/symtable.h
+++ trunk/src/front-end/symtable.h
@@ -1,14 +1,25 @@
 #ifndef __SYMTABLEH__
 #define __SYMTABLEH__
+
+/* Define const for types */
+#define TYPE_UNDEF -4
+#define TYPE_VOID -3
+#define TYPE_INT -2
+#define TYPE_FLOAT -1
+//Every type >= 0 is a function with n parameters
+
+
 typedef struct Node{
 	int type;
-	void* value;
+	void* addr;
 	char* name;
 	struct Node* next;
 } Node;
 
 
-Node* add_to_symtable(Node n, Node* list);
+Node* create_symtable(Node n);
+Node* add_start_to_symtable(Node n, Node* list);
+Node* add_end_to_symtable(Node n, Node* list);
 int find_in_symtable(char* n,const Node* ln);
 void free_symtable(Node* ln);
 #endif
