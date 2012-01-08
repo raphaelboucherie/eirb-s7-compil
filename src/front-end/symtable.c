80473aac37675a06b668a7e4c4767da75085f7fd
laturaze.paul <laturaze.paul@gmail.com>
2012-01-08 13:38:58 +0000

    Modification du module de table des symboles

--- src/front-end/symtable.c
+++ src/front-end/symtable.c
@@ -1,16 +1,56 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "symtable.h"
 
-Node* add_to_symtable(Node n, Node* list){
+/* Crée une liste pour la table des symbole à partir d'un premier élément */
+Node* create_symtable(Node n){
 	Node* newNode = malloc(sizeof(Node));
-	newNode->type = n.type;
-	newNode->value = n.value; /* Malloc la value selon le type */
-	newNode->name = strdup(n.name);
-	newNode->next = list;
-	return newNode;	
+	if(newNode != NULL){
+		newNode->type = n.type;
+		newNode->addr = n.addr; /* Malloc la value selon le type */
+		newNode->name = strdup(n.name);
+		newNode->next = NULL;
+		return newNode;
+	}
+	perror("Allocation error\n");
+	exit(1);
 }
 
+/* Ajout un élément en début de la liste */
+Node* add_start_to_symtable(Node n, Node* list){
+	Node* newNode = malloc(sizeof(Node));
+	if(newNode != NULL){
+		newNode->type = n.type;
+		newNode->addr = n.addr; /* Malloc la value selon le type */
+		newNode->name = strdup(n.name);
+		newNode->next = list;
+		return newNode;	
+	}
+	perror("Allocation error");
+	exit(1);
+}
+
+/* Ajoute un élément à la fin de la liste */
+Node* add_end_to_symtable(Node n, Node* ln){
+	Node* list = ln;
+	Node* newNode = malloc(sizeof(Node));
+	if(newNode != NULL){
+		newNode->type = n.type;
+		newNode->addr = n.addr; /* Malloc la value selon le type */
+		newNode->name = strdup(n.name);
+		newNode->next = NULL;
+		while(list->next != NULL){
+			list = list->next;
+		}
+		list->next = newNode;
+		return ln;
+	}
+	perror("Allocation error");
+	exit(1);
+}
+
+/* Cherche l'élément de nom n dans la liste */
 int find_in_symtable(char* n, const Node* ln){
 	while(ln != NULL && ln->next != NULL){
 		if(strcmp(ln->name, n) == 0){
@@ -21,15 +61,15 @@ int find_in_symtable(char* n, const Node* ln){
 	return 0;
 }
 
+/* destruction de la table des symbole */
 void free_symtable(Node* ln){
+	Node* ln_tmp;
 	if(ln != NULL){
-		Node* ln_tmp;
-		while(ln->next != NULL){
+		while(ln != NULL){
 			ln_tmp = ln->next;
 			free(ln->name);
 			free(ln);
 			ln = ln_tmp;
 		}
-		free(ln);
 	}
 }
