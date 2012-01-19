7dcf0e71a2b18a538e5e3737cdc964191c3c3511
laturaze.paul@gmail.com <laturaze.paul@gmail.com@a4e27ffd-a61e-4b3a-5b61-575e9ed28477>
2012-01-19 09:45:53 +0000

    header pour la génération de code 2 adresses

---
+++ trunk/src/front-end/generate2a.h
@@ -0,0 +1,10 @@
+#ifndef __GENERATE_2A_H__
+#define __GENERATE_2A_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "derivationtree.h"
+
+void tree_to_2a_code(TreeNode* tn);
+
+#endif /* __GENERATE_2A_H__ */
