37ff15744e4a421e03fb19b12f74757d0ebf7897
rdestigny <rdestigny@gmail.com>
2012-01-21 02:30:16 +0000

    DORMIR

--- src/back-end/globals.h
+++ src/back-end/globals.h
@@ -5,9 +5,8 @@
 #include "symbolTable.h"
 #include "stringList.h"
 
-/* SYMBOL TABLE PART */
 
-//struct symT* symbolTable = NULL;    
+
 
 static const int type_UNDEFINED = 0x1;
 static const int type_FLOAT = 0x10;
@@ -15,27 +14,16 @@ static const int type_INT = 0x100;
 static const int type_ARRAY = 0x1000;
 static const int type_FUNCTION = 0x10000;
 
-// everything from 0 to n is a function with n parameter
-
+static const int operator_ASSIGN = 0x1;
+static const int operator_MUL = 0x10;
+static const int operator_ADD = 0x100;
+static const int operator_SUB = 0x1000;
 
 struct symbolTableTreeNode *symbolTableRoot = NULL;
 struct symbolTableTreeNode *symbolTableCurrentNode = NULL;
 
 
-/***********************/
-
-/* LABEL MANAGEMENT PART */
-
 static struct pile* labelPile = NULL;
-
-/************************/
-
-/* DECLARATOR LIST MANAGEMENT PART */
-
-
-
-/**********************************/
-
 
 struct string* asmCode;
 
